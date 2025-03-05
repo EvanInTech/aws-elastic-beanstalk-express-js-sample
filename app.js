6d7619518b42d92c5552ec843d76b4ad2342f266
Evan Williams <79871203+EvanInTech@users.noreply.github.com>
2025-03-05 15:14:14 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is Evan working with continuous delivery pipeline'));
+app.get('/', (req, res) => res.send('This is Evan working with continuous delivery pipeline. Adding some text here to test pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
